e5b41196e53792850a8c187ac2fe412013ae4699
hana-branch <hpark@branch.io>
2021-05-13 12:38:52 -0700

    update

---
+++ src/Subscribe.js
@@ -0,0 +1,14 @@
+import branch from 'react-native-branch'
+
+console.info("Subscribing to Branch links")
+
+branch.subscribe(({ error, params }) => {
+  if (error) {
+    console.error("Error from Branch: " + error)
+    return
+  }
+
+  console.info("Received link response from Branch")
+
+  console.log("Branch Params: " + JSON.stringify(params))
+})
